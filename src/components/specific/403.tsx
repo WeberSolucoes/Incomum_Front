f98cc7fd296c269eab80c5a09726501d86539d16
BrunoMartins <brunomartins616@hotmail.com>
2024-07-31 08:51:10 -0300

    botoes voltando para a main page

--- src/components/specific/403.tsx
+++ src/components/specific/403.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../../assets/styles/pages/403.css';
+import { useNavigate } from 'react-router-dom';
 const NotAuthorize = () => {
+  const navigate = useNavigate();
   return (
     <div style={{ height: '100vh' }}>
       <div className="not-found-container">
@@ -11,7 +13,7 @@ const NotAuthorize = () => {
           </div>
           <p>Não autorizado. <br /> Caso não esteja conseguindo acessar a página, entrar em contato com suporte.</p>
           <a href="/">
-            <button type="button">Voltar</button>
+            <button type="button" onClick={(e) => { e.preventDefault(); navigate('/')}}>Voltar</button>
           </a>
         </div>
       </div>
