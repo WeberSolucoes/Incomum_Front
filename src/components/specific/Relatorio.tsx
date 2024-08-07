c610f0aebbf3178cc35a3f9194c44dbaacd9b452
WeberSolucoes <gilberto@webersolucoes.com.br>
2024-08-07 15:19:28 -0300

    formatacao_relatorio

--- src/components/specific/Relatorio.tsx
+++ src/components/specific/Relatorio.tsx
@@ -195,7 +195,7 @@ const Relatorio = () => {
                 <Column field="fim_valorinc" header="Inc" />
                 <Column field="fim_valorincajustado" header="Inc Ajustado" />
                 <Column field="aco_descricao" header="Área Comercial" />
-                <Column field="nome_loja" header="Agência" />
+                <Column field="age_descricao" header="Agência" />
             </DataTable>
         </div>
     );
