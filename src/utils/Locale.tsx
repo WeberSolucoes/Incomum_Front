74688370dc67a032da7ff8a759dd99003de1b5e7
BrunoMartins <brunomartins616@hotmail.com>
2024-08-07 15:26:25 -0300

    Merge branch 'sprint' of https://github.com/WeberSolucoes/incomum2_0_front into tela_relatorio

---
+++ src/utils/Locale.tsx
@@ -0,0 +1,13 @@
+import { addLocale } from "primereact/api";
+
+addLocale('pt-br', {
+    firstDayOfWeek: 1,
+    dayNames: ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado'],
+    dayNamesShort: ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sáb'],
+    dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+    monthNames: ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'],
+    monthNamesShort: ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'],
+    today: 'Hoje',
+    clear: 'Limpar'
+});
+export const pt_br = 'pt-br';
