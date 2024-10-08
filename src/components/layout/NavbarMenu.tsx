82ac03747c074404fb481e08efe991cad7c47eb5
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-10-08 08:18:36 -0300

    Update NavbarMenu.tsx

--- src/components/layout/NavbarMenu.tsx
+++ src/components/layout/NavbarMenu.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import { Menubar } from 'primereact/menubar';
 import { useAuth } from '../../contexts/AuthProvider';
+import { Button } from 'primereact/button';
+import { useNavigate } from 'react-router-dom';
 
 const NavbarMenu: React.FC = () => {
-    useAuth();
+    const auth = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        if (window.confirm('Você tem certeza que deseja sair?')) {
+            auth.logout(); // Chama a função de logout do contexto
+            navigate('/login'); // Redireciona para a página de login
+        }
+    };
+
     return (
         <Menubar
-            style={{ zIndex: 1000, position: 'static', top: 0, width: '100%', boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',backgroundColor:'#0152a1',height:'50px' }}
+            end={<Button label="Sair" icon="pi pi-sign-out" onClick={handleLogout} className="p-button-danger" />}
+            style={{
+                zIndex: 1000,
+                position: 'static',
+                top: 0,
+                width: '100%',
+                boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+                backgroundColor: '#0152a1',
+                height: '50px',
+            }}
         />
     );
 };
