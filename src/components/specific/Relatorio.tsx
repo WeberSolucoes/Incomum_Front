6826478ce3c7a0bff01340c2767d78526a62610d
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-10-08 08:13:09 -0300

    Update Relatorio.tsx

--- src/components/specific/Relatorio.tsx
+++ src/components/specific/Relatorio.tsx
@@ -1,23 +1,13 @@
 import { Calendar } from 'primereact/calendar';
-//import { FloatLabel } from 'primereact/floatlabel';
 import { useEffect, useState } from 'react';
-import { Paginator } from 'primereact/paginator';
 import { Button } from 'primereact/button';
+import { MultiSelect } from 'primereact/multiselect';
+import { Dropdown } from 'primereact/dropdown';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { MultiSelect } from 'primereact/multiselect';
-import {
-    apiGetAgenciaRelatorioByUser,
-    apiGetAreaComercialRelatorioByUser,
-    apiGetDownloadRelatorio,
-    apiGetRelatorioFindByFilter,
-    apiGetTotalRelatorio,
-    apiGetUnidadeRelatorioByUser,
-    apiGetUserId,
-    apiGetVendedorRelatorioByUser
-} from '../../services/Api';
-import { pt_br } from '../../utils/Locale';
-import { toastError, toastWarning } from '../../utils/customToast';
+import { apiGetRelatorioFindByFilter, apiGetUnidadeRelatorioByUser, apiGetAreaComercialRelatorioByUser, apiGetAgenciaRelatorioByUser, apiGetVendedorRelatorioByUser } from '../../services/Api';
+import { toastError,} from '../../utils/customToast';
+import axios from 'axios';
 
 
 const Relatorio = () => {
@@ -25,11 +15,11 @@ const Relatorio = () => {
     const [areasComerciais, setAreasComerciais] = useState([]);
     const [agencias, setAgencias] = useState([]);
     const [vendedores, setVendedores] = useState([]);
-
-    const [selectedUnidade, setSelectedUnidade] = useState([]);
+    
+    const [selectedUnidade, setSelectedUnidade] = useState(null);
     const [selectedAreaComercial, setSelectedAreaComercial] = useState([]);
-    const [selectedAgencia, setSelectedAgencia] = useState([]);
-    const [selectedVendedor, setSelectedVendedor] = useState([]);
+    const [selectedVendedor, setSelectedVendedor] = useState(null); 
+    const [selectedAgencia, setSelectedAgencia] = useState(null); 
 
     const [loading, setLoading] = useState(false);
     const [dateStart, setDateStart] = useState(null);
@@ -42,28 +32,26 @@ const Relatorio = () => {
         loadDadosIniciais();
     }, []);
 
+    const [totals, setTotals] = useState({
+        totalValorInc: 0,
+        totalValorIncAjustado: 0,
+        totalValorLiquido: 0
+    });
+
     const loadDadosIniciais = async () => {
+        setLoading(true);
         try {
-            setLoading(true);
             const [unidadesResponse, areasResponse, agenciasResponse, vendedoresResponse] = await Promise.all([
                 apiGetUnidadeRelatorioByUser(),
                 apiGetAreaComercialRelatorioByUser(),
                 apiGetAgenciaRelatorioByUser(),
                 apiGetVendedorRelatorioByUser(),
             ]);
-    
-            console.log('Respostas da API:', { 
-                unidadesResponse, 
-                areasResponse, 
-                agenciasResponse, 
-                vendedoresResponse 
-            });
-    
-            // Mapear e extrair apenas os valores desejados
+
             setUnidades(unidadesResponse.data.map(item => ({ label: item.loj_descricao, value: item.loj_codigo })));
-            setAreasComerciais(areasResponse.data.map(item => ({ label: item.aco_descricao, value: item.aco_codigo })));
-            setAgencias(agenciasResponse.data.map(item => ({ label: item.age_descricao, value: item.age_codigo })));
-            setVendedores(vendedoresResponse.data.map(item => ({ label: item.ven_descricao, value: item.ven_codigo })));
+            setAreasComerciais(areasResponse.data.associacoes.map(item => ({ label: item.aco_descricao, value: item.aco_codigo })));
+            setAgencias(agenciasResponse.data.valores.map(item => ({ label: item.age_descricao, value: item.age_codigo })));
+            setVendedores(vendedoresResponse.data.vende.map(item => ({ label: item.ven_descricao, value: item.ven_codigo })));
         } catch (error) {
             toastError('Erro ao carregar os dados iniciais');
         } finally {
@@ -74,31 +62,44 @@ const Relatorio = () => {
     const handleSubmit = async (event) => {
         event.preventDefault();
         setData([]);
-
-        // Verifica se as datas são válidas
+        
         if (!dateStart || !dateEnd) {
             toastError('As datas de início e fim são obrigatórias.');
             return;
         }
-
+    
         const params = {
             dataInicio: dateStart.toISOString().split('T')[0],
             dataFim: dateEnd.toISOString().split('T')[0],
-            unidades: selectedUnidade.length > 0 ? selectedUnidade : [],
+            unidades: selectedUnidade ? [selectedUnidade] : [], 
             areasComerciais: selectedAreaComercial.length > 0 ? selectedAreaComercial : [],
-            agencias: selectedAgencia.length > 0 ? selectedAgencia : [],
-            vendedores: selectedVendedor.length > 0 ? selectedVendedor : [],
+            agencias: selectedAgencia ? [selectedAgencia] : [],
+            vendedores: selectedVendedor ? [selectedVendedor] : [],
         };
-
-        console.log('Parâmetros enviados para a API:', params);
-
+    
         try {
             setTableLoading(true);
             const response = await apiGetRelatorioFindByFilter(params);
-
-            if (Array.isArray(response.data) && response.data.length > 0) {
-                setData(response.data);
-                setTotal(response.data.length);
+    
+            if (response.data && Array.isArray(response.data.resultados) && response.data.resultados.length > 0) {
+                setData(response.data.resultados);
+                setTotal(response.data.resultados.length);
+    
+                // Cálculo dos totais
+                const totalValorInc = response.data.resultados.reduce((sum, item) => sum + item.fim_valorinc, 0);
+                const totalValorIncAjustado = response.data.resultados.reduce((sum, item) => sum + item.fim_valorincajustado, 0);
+                const totalValorLiquido = response.data.resultados.reduce((sum, item) => sum + item.fim_valorliquido, 0);
+    
+                // Formatação dos totais
+                const totalValorIncFormatado = totalValorInc.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+                const totalValorIncAjustadoFormatado = totalValorIncAjustado.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+                const totalValorLiquidoFormatado = totalValorLiquido.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    
+                setTotals({
+                    totalValorInc: totalValorIncFormatado,
+                    totalValorIncAjustado: totalValorIncAjustadoFormatado,
+                    totalValorLiquido: totalValorLiquidoFormatado
+                });
             } else {
                 setData([]);
                 setTotal(0);
@@ -112,59 +113,153 @@ const Relatorio = () => {
         }
     };
 
+    const handleExport = async () => {
+        try {
+            const response = await axios.get('http://3.21.123.210:8443/api/incomum/relatorio/download-relatorio/', {
+                params: {
+                    dataInicio: dateStart?.toISOString().split('T')[0], // Adicionei a formatação
+                    dataFim: dateEnd?.toISOString().split('T')[0],
+                    unidade: selectedUnidade, // Envia apenas a unidade selecionada
+                    areaComercial: selectedAreaComercial,
+                    agencia: selectedAgencia,
+                    vendedor: selectedVendedor,
+                },
+                responseType: 'blob',
+            });
+
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', 'relatorio.xlsx');
+            document.body.appendChild(link);
+            link.click();
+            link.remove();
+        } catch (error) {
+            console.error('Erro ao exportar Excel:', error);
+            toastError('Erro ao exportar Excel');
+        }
+    };
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
+    const scrollToBottom = () => {
+        window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+    };
 
     return (
         <>
-            <div style={{backgroundColor:'white',borderRadius:'10px',marginTop:'-40px',padding:'inherit',width: '1000px',marginLeft: '300px'}} className='container px-4'>
-                <form style={{ backgroundColor: '#f9f9f9', width: '100%', margin: 'auto', padding: '20px', border: '1px solid #ddd', borderRadius: '5px',height:'90%' }} action="">
-                    <div className='row mt-3'>
-                        <h1 style={{marginTop:'-15px',color:'#0152a1'}}>Relatório</h1>
-                        <div className='col-sm-6 mb-3'>
-                            <Calendar locale={pt_br} style={{ width: '100%',marginTop:'15px' }} value={dateStart} onChange={(e: any) => setDateStart(e.value)} showIcon placeholder="Data Inicial" dateFormat='dd/mm/yy' className="custom-calendar"  />
-                        </div>
-                        <div className='col-sm-6 mb-3'>
-                            <Calendar locale={pt_br} style={{ width: '100%', marginTop:'15px' }} value={dateEnd} onChange={(e: any) => setDateEnd(e.value)} showIcon placeholder="Data Final" dateFormat='dd/mm/yy' className="custom-calendar"  />
-                        </div>
+        <div style={{backgroundColor:'white',borderRadius:'10px',marginTop:'-40px',padding:'inherit',width: '1100px',marginLeft: '320px', boxShadow: '10px 10px 100px rgba(0, 0, 0, 0.4),-2px -2px 6px rgba(255, 255, 255, 0.6)'}} className="container px-4">
+            <form style={{ backgroundColor: '#f9f9f9', width: '100%', margin: 'auto', padding: '20px', border: '1px solid #ddd', borderRadius: '5px',height:'90%' }} onSubmit={handleSubmit}>
+                <div className='row mt-3'>
+                    <h1 style={{ marginTop: '-15px', color: '#0152a1' }}>Relatório</h1>
+                    <div className='col-sm-6 mb-3'>
+                        <Calendar value={dateStart} onChange={(e) => setDateStart(e.value)} showIcon placeholder="Data Inicial" />
                     </div>
-                    <div className='row mt-3'>
-                        <div className='col-sm-3 mb-3'>
-                            <MultiSelect value={selectedUnidade} style={{ width: '100%' }} showClear loading={loading}
-                                options={unidades} filter emptyFilterMessage='Nenhum registro encontrado' emptyMessage='Nenhum registro encontrado'
-                                onChange={(e) => handleSelectionChange('unidade', e.value)} optionLabel="name" placeholder="Unidade" className="w-full md:w-14rem" />
-                        </div>
-                        <div className='col-sm-3 mb-3'>
-                            <MultiSelect value={selectedAreaComercial} style={{ width: '100%' }} showClear loading={loading}
-                                options={areasComerciais} filter emptyFilterMessage='Nenhum registro encontrado' emptyMessage='Nenhum registro encontrado'
-                                onChange={(e) => handleSelectionChange('areaComercial', e.value)} optionLabel="name" placeholder="Área Comercial" className="w-full md:w-14rem" />
-                        </div>
-                        <div className='col-sm-3 mb-3'>
-                            <MultiSelect value={selectedAgencia} style={{ width: '100%' }} showClear loading={loading}
-                                options={agencias} filter emptyFilterMessage='Nenhum registro encontrado' emptyMessage='Nenhum registro encontrado'
-                                onChange={(e) => handleSelectionChange('agencia', e.value)} optionLabel="name" placeholder="Agência" className="w-full md:w-14rem" />
-                        </div>
-                        <div className='col-sm-3 mb-3'>
-                            <MultiSelect value={selectedVendedor} style={{ width: '100%' }} showClear loading={loading} onClick={handleVendedoresValues}
-                                options={vendedores} filter emptyFilterMessage='Nenhum registro encontrado' emptyMessage='Nenhum registro encontrado'
-                                onChange={(e) => handleSelectionChange('vendedor', e.value)} optionLabel="name" placeholder="Vendedor" className="w-full md:w-14rem" />
-                        </div>
+                    <div className='col-sm-6 mb-3'>
+                        <Calendar style={{marginLeft:'-258px'}} value={dateEnd} onChange={(e) => setDateEnd(e.value)} showIcon placeholder="Data Final" />
                     </div>
-                    <div className="row my-3 d-flex justify-content-end align-items-center">
-                        <Button style={{margin:'auto',backgroundColor:'#0152a1'}} className='rounded col-sm-2 mb-3' id='pesquisar' loading={tableLoading} label="Pesquisar" icon="pi pi-search" onClick={handleSubmit} />
+                </div>
+                <div className='row mt-3'>
+                    <div className='col-sm-3 mb-3'>
+                        <Dropdown
+                            value={selectedUnidade} 
+                            options={unidades} 
+                            onChange={(e) => setSelectedUnidade(e.value)} 
+                            placeholder="Unidade"
+                            style={{width:'100%'}}
+                            panelStyle={{ width: '10%' }} // Largura do painel  
+                        />
                     </div>
-                    <DataTable removableSort loading={tableLoading} scrollable scrollHeight="500px" emptyMessage="Nenhum registro encontrado" value={data} tableStyle={{ minWidth: '10rem' }} paginator rows={10} totalRecords={total} >
-                        <Column sortable field="fim_tipo" header="Tipo" />
-                        <Column sortable field="tur_numerovenda" header="Núm. Venda" />
-                        <Column sortable field="tur_codigo" header="Num. Pct" />
-                        <Column sortable field="fim_valorliquido" header="Vlr Líq Venda" />
-                        <Column sortable field="fim_data" header="Data" />
-                        <Column sortable field="fim_markup" header="Mkp" />
-                        <Column sortable field="fim_valorinc" header="Inc" />
-                        <Column sortable field="fim_valorincajustado" header="Inc Ajustado" />
-                        <Column sortable field="aco_descricao" header="Área Comercial" />
-                        <Column sortable field="nome_loja" header="Agência" />
-                        <Column sortable field="ven_descricao" header="Vendedor" />
+                    <div className='col-sm-3 mb-3'>
+                        <MultiSelect 
+                            value={selectedAreaComercial} 
+                            options={areasComerciais} 
+                            onChange={(e) => setSelectedAreaComercial(e.value)} 
+                            placeholder="Área Comercial" 
+                            display="chip" 
+                            style={{width:'100%'}}
+                            panelStyle={{ width: '100%' }} // Largura do painel
+                        />
+                    </div>
+                    <div className='col-sm-3 mb-3'>
+                        <Dropdown 
+                            value={selectedAgencia} 
+                            options={agencias} 
+                            onChange={(e) => setSelectedAgencia(e.value)} 
+                            placeholder="Agência" 
+                            style={{width:'100%'}} 
+                        />
+                    </div>
+                    <div className='col-sm-3 mb-3'>
+                        <Dropdown 
+                            value={selectedVendedor} 
+                            options={vendedores} 
+                            onChange={(e) => setSelectedVendedor(e.value)} 
+                            placeholder="Vendedor"
+                            style={{width:'100%'}}
+                        />
+                    </div>
+                </div>
+                <div className='row mt-3'>
+                    <div className='col-12 d-flex justify-content-end'>
+                        <Button 
+                            style={{marginRight:'8px',backgroundColor:'#1d6f42',border:'none'}}
+                            icon="pi pi-file-excel" 
+                            onClick={handleExport}
+                            className="custom-button" // Estilos adicionais, se necessário
+                        />
+                        <Button style={{backgroundColor:'#0152a1'}} type="submit" label="Consultar" icon="pi pi-search" />
+                    </div>
+                </div>
+            </form>
+
+            <div className='row mt-5'>
+                <div className='col-12'>
+                    <DataTable 
+                        value={data} 
+                        loading={tableLoading} 
+                        totalRecords={total} 
+                        responsiveLayout="scroll"
+                    >
+                        <Column field="fim_codigo" header="Código" />
+                        <Column field="fim_data" header="Data" />
+                        <Column field="fim_markup" header="Markup" />
+                        <Column field="fim_tipo" header="Tipo" />
+                        <Column field="fim_valorinc" header="Valor Inicial" />
+                        <Column field="fim_valorincajustado" header="Valor Ajustado" />
+                        <Column field="fim_valorliquido" header="Valor Líquido" />
+                        <Column field="tur_codigo" header="Tur Código" />
+                        <Column field="tur_numerovenda" header="Número da Venda" />
                     </DataTable>
-                </form>
+                    <div style={{ display: 'flex', justifyContent: 'space-between', backgroundColor: '#f0f0f0', fontWeight: 'bold', padding: '10px 0' }}>
+                        <div>Total:</div>
+                        <div>Valor Inicial: {totals.totalValorInc.toFixed(2)}</div>
+                        <div>Valor Ajustado: {totals.totalValorIncAjustado.toFixed(2)}</div>
+                        <div>Valor Líquido: {totals.totalValorLiquido.toFixed(2)}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+          {/* Botões de rolagem */}
+          <div style={{
+                position: 'fixed',
+                bottom: '20px',
+                right: '20px',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '10px'
+            }}>
+                <Button
+                    icon="pi pi-arrow-up"
+                    onClick={scrollToTop}
+                    style={{ backgroundColor: '#0152a1', border: 'none',borderRadius:'10px' }}
+                />
+                <Button
+                    icon="pi pi-arrow-down"
+                    onClick={scrollToBottom}
+                    style={{ backgroundColor: '#0152a1', border: 'none',borderRadius:'10px' }}
+                />
             </div>
         </>
     );
