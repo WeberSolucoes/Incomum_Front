fd9a08bf2ec31e0ee748fbc40939d353b8be13c1
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-10-08 08:16:49 -0300

    Update AuthProvider.tsx

--- src/contexts/AuthProvider.tsx
+++ src/contexts/AuthProvider.tsx
@@ -1,91 +1,80 @@
-// src/context/AuthContext.tsx
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import {
-    apiPostlogin as apiLogin, 
-    logout as apiLogout, 
-    setToken as apiSetToken, 
-    getToken as apiGetToken,
-    getPermissions as apiGetPermissions,
-    setPermissions as apiSetPermissions,
-    apiGetUserId as apiUserId,
-    apiGetPermissions as apiPermissions
-} from '../services/Api';
-import { LoginResponse, PermissionsListResponse } from '../utils/apiObjects';
+import axios from 'axios';
 
 interface AuthContextType {
-    isAuthenticated: boolean;
-    userPermissions: PermissionsListResponse[];
-    login: (email: string, password: string, rememberMe: boolean) => Promise<void>;
+    login: (email: string, senha: string, remember: boolean) => Promise<void>;
     logout: () => void;
-    loading: boolean;
-    checkAuth: () => Promise<boolean>;
+    isAuthenticated: boolean;
+    token: string | null;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-interface AuthProviderProps {
-    children: React.ReactNode;
-}
-export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-    const [userPermissions, setUserPermissions] = useState<PermissionsListResponse[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
+    const [token, setToken] = useState<string | null>(null);
 
+    // Efeito para verificar o token ao carregar a página
     useEffect(() => {
-        const token = apiGetToken();
-        if (token) {
-            setIsAuthenticated(true);
-            // Defini as permissoes do usuario se existirem            
-            const permissions = apiGetPermissions() as PermissionsListResponse[];
-            setUserPermissions(permissions);
+        const savedToken = localStorage.getItem('token') || sessionStorage.getItem('token');
+        const expiration = localStorage.getItem('authExpiration');
+
+        if (savedToken && expiration) {
+            const isExpired = Date.now() > Number(expiration);
+            if (isExpired) {
+                logout(); // Faz logout se o token expirou
+            } else {
+                setIsAuthenticated(true); // Mantém o estado de autenticação
+                setToken(savedToken); // Salva o token
+            }
         }
-        setLoading(false);
     }, []);
-    const checkAuth = async () => {
-        const isAuthenticated = await apiGetToken(); // ou qualquer método assíncrono
-        if(isAuthenticated) return true;
-        return false;
-    }
-    const login = async (email: string, password: string, rememberMe: boolean) => {
+
+    const login = async (email: string, senha: string, remember: boolean) => {
         try {
-            //Pega o token
-            const response = await apiLogin({ email, password });
-            const responseData: LoginResponse = response.data;
-            //Define o token
-            apiSetToken(responseData.access, rememberMe);
-            setIsAuthenticated(true);
+            const response = await axios.post('http://127.0.0.1:8000/api/incomum/usuario/login/', {
+                loginemail: email,
+                loginsenha: senha,
+            });
 
-            // Pega o userId
-            const userId = await apiUserId();
-            const userIdResponse:number = userId.data.id;
+            if (response.data.access) {
+                const token = response.data.access;
+                setIsAuthenticated(true);
+                setToken(token);
 
-            const permissionResponse = await apiPermissions(userIdResponse);
-            const permissions: PermissionsListResponse[] = permissionResponse.data;
-            setUserPermissions(permissions);
-            apiSetPermissions(permissions, rememberMe);
+                if (remember) {
+                    localStorage.setItem('token', token);
+                    const expirationTime = Date.now() + 2 * 60 * 60 * 1000; // 2 horas
+                    localStorage.setItem('authExpiration', expirationTime.toString());
+                } else {
+                    sessionStorage.setItem('token', token);
+                }
+            } else {
+                throw new Error(response.data.error_message || 'Erro ao efetuar login');
+            }
         } catch (error) {
-            setIsAuthenticated(false);
-            setUserPermissions([]);
-            throw new Error('Erro ao fazer login');
+            throw new Error('Erro ao efetuar login: ' + (error instanceof Error ? error.message : 'Erro desconhecido'));
         }
     };
 
     const logout = () => {
-        apiLogout();
         setIsAuthenticated(false);
-        setUserPermissions([]);
+        setToken(null);
+        localStorage.removeItem('token');
+        localStorage.removeItem('authExpiration');
+        sessionStorage.removeItem('token');
     };
 
     return (
-        <AuthContext.Provider value={{ isAuthenticated, userPermissions, login, logout, loading, checkAuth }}>
+        <AuthContext.Provider value={{ login, logout, isAuthenticated, token }}>
             {children}
         </AuthContext.Provider>
     );
 };
 
-export const  useAuth = () => {
+export const useAuth = (): AuthContextType => {
     const context = useContext(AuthContext);
-    if (!context) {
+    if (context === undefined) {
         throw new Error('useAuth must be used within an AuthProvider');
     }
     return context;
