2c5f97713616cc12c7399b91167f436b774ab601
Ygor <86171558+ygor84@users.noreply.github.com>
2024-10-16 17:07:19 -0300

    Update BarraDeFerramentas.tsx

--- src/components/common/BarraDeFerramentas.tsx
+++ src/components/common/BarraDeFerramentas.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Toolbar } from 'primereact/toolbar';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
@@ -8,45 +8,26 @@ import { InputIcon } from 'primereact/inputicon';
 interface BarraDeFerramentasProps {
     onCreateClick: () => void;
     onSearch: (term: string) => void;
+    resetSearch: boolean; // Nova prop para resetar o campo de busca
 }
 
-const BarraDeFerramentas: React.FC<BarraDeFerramentasProps> = ({ onCreateClick, onSearch }) => {
+const BarraDeFerramentas: React.FC<BarraDeFerramentasProps> = ({ onCreateClick, onSearch, resetSearch }) => {
     const [searchTerm, setSearchTerm] = useState(''); // Estado para armazenar o termo de busca
+
+    // Limpa o termo de busca toda vez que a prop resetSearch mudar
+    useEffect(() => {
+        if (resetSearch) {
+            setSearchTerm(''); // Reseta o termo de busca
+        }
+    }, [resetSearch]); // Executa quando resetSearch mudar
 
     // Função para lidar com o clique no botão de consulta
     const handleSearchClick = () => {
         onSearch(searchTerm); // Dispara a pesquisa com o termo armazenado
     };
 
-    const endContent = (
-        <React.Fragment>
-            <Button style={{ backgroundColor: '#001a40', border: 'none' }} icon="pi pi-plus" iconPos='right' label='Criar' className="rounded" onClick={onCreateClick} />
-        </React.Fragment>
-    );
-
-    const centerContent = (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-            <IconField iconPosition="left">
-                <InputIcon className="pi pi-search" style={{ position: 'absolute', left: '10px', top: '70%', transform: 'translateY(-50%)' }} />
-                <InputText
-                    style={{ width: '300px', paddingLeft: '2.5rem' }} // Ajuste do paddingLeft para deixar espaço para o ícone
-                    placeholder="Buscar"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)} // Atualiza o termo de busca no estado
-                />
-            </IconField>
-            <Button 
-                icon="pi pi-search" 
-                label="Consultar" 
-                className="p-button-rounded p-button-secondary" 
-                onClick={handleSearchClick} // Chama a pesquisa ao clicar no botão
-                style={{ marginLeft: '10px',backgroundColor:'#0152a1',borderRadius:'4px' }} // Espaço entre o input e o botão
-            />
-        </div>
-    );
-
     return (
-        <Toolbar start={centerContent} end={endContent} />
+        <div></div>
     );
 };
 
