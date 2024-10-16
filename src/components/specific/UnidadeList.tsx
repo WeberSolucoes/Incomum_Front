b9560dc93a10f13f43da0984c26e869b1c8dd3da
Ygor <86171558+ygor84@users.noreply.github.com>
2024-10-16 17:03:08 -0300

    Update UnidadeList.tsx

--- src/components/specific/UnidadeList.tsx
+++ src/components/specific/UnidadeList.tsx
@@ -3,14 +3,18 @@ import { UnidadesListResponse } from '../../utils/apiObjects';
 import GenericTable from '../common/GenericTable';
 import { apiGetUnidades } from '../../services/Api';
 import { toastError } from '../../utils/customToast';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { useCodigo } from '../../contexts/CodigoProvider'; // Importa o contexto
+import UnidadeCadastro from './UnidadeCadastro'; // Importa o componente de cadastro
 
-interface UnidadeListProps {
-    search: string; // Termo de pesquisa passado como prop
-}
-
-const UnidadeList: React.FC<UnidadeListProps> = ({ search }) => {
+const UnidadeListConsolidada: React.FC = () => {
     const [items, setItems] = useState<UnidadesListResponse[]>([]);
-    const [originalItems, setOriginalItems] = useState<UnidadesListResponse[]>([]); // Para armazenar os dados originais
+    const [originalItems, setOriginalItems] = useState<UnidadesListResponse[]>([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [view, setView] = useState<'list' | 'create'>('list'); // Estado para controlar a visualização atual
+
+    const { setCodigo } = useCodigo(); // Acesso ao contexto
 
     useEffect(() => {
         const fetchData = async () => {
@@ -19,35 +23,79 @@ const UnidadeList: React.FC<UnidadeListProps> = ({ search }) => {
                 const mappedData: UnidadesListResponse[] = response.data.map((item: any) => ({
                     codigo: item.loj_codigo,
                     descricao: item.loj_descricao,
-                    responsavel: item.loj_email, // Campos do objeto que você espera
-                    email: item.loj_cnpj, // Campos do objeto que você espera
+                    responsavel: item.loj_email,
+                    email: item.loj_cnpj,
                 }));
                 setItems(mappedData);
-                setOriginalItems(mappedData); // Armazena os dados originais
+                setOriginalItems(mappedData);
             } catch (error) {
-                toastError('Erro ao buscar as agências');
+                toastError('Erro ao buscar as unidades');
             }
         };
 
         fetchData();
     }, []);
 
-    // Filtra os itens com base na pesquisa
-    const filteredItems = search
-        ? originalItems.filter(item => {
-              const searchTerm = search.toLowerCase();
-              return (
-                  item.descricao.toLowerCase().includes(searchTerm) ||
-                  item.codigo.toString().toLowerCase().includes(searchTerm) ||
-                  (item.responsavel && item.responsavel.toLowerCase().includes(searchTerm)) ||
-                  (item.email && item.email.toString().toLowerCase().includes(searchTerm))
-              );
-          })
-        : []; // Exibe uma lista vazia se não houver pesquisa
+    const handleSearch = () => {
+        if (searchTerm) {
+            const searchTermLower = searchTerm.toLowerCase();
+            const filteredItems = originalItems.filter(item =>
+                item.descricao.toLowerCase().includes(searchTermLower) ||
+                item.codigo.toString().toLowerCase().includes(searchTermLower) ||
+                (item.responsavel && item.responsavel.toLowerCase().includes(searchTermLower)) ||
+                (item.email && item.email.toLowerCase().includes(searchTermLower))
+            );
+            setItems(filteredItems);
+        } else {
+            setItems(originalItems);
+        }
+    };
+
+    const handleCodeClick = (codigo: number) => {
+        setCodigo(codigo);
+        setView('create'); // Muda para a visualização de edição
+    };
+
+    const handleCreateClick = () => {
+        setCodigo(null); // Resetando o código para criar uma nova unidade
+        setView('create'); // Muda para a visualização de criação
+    };
 
     return (
-        <GenericTable filteredItems={filteredItems} emptyMessage='Nenhuma agência encontrada' />
+        <div>
+            {view === 'list' ? ( // Verifica qual view deve ser renderizada
+                <>
+                    <div style={{ display: 'flex', alignItems: 'center', marginBottom: '1rem' }}>
+                        <InputText
+                            style={{ width: '300px' }}
+                            placeholder="Buscar"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                        />
+                        <Button
+                            label="Consultar"
+                            icon="pi pi-search"
+                            style={{ marginLeft: '10px', backgroundColor: '#0152a1' }}
+                            onClick={handleSearch}
+                        />
+                        <Button
+                            label="Criar"
+                            icon="pi pi-plus"
+                            style={{ marginLeft: 'auto', backgroundColor: '#001a40' }}
+                            onClick={handleCreateClick} // Chama a função de criação ao clicar no botão
+                        />
+                    </div>
+                    <GenericTable 
+                        filteredItems={items} 
+                        emptyMessage="Nenhuma unidade encontrada" 
+                        onCodeClick={handleCodeClick} 
+                    />
+                </>
+            ) : (
+                <UnidadeCadastro /> // Renderiza o componente de cadastro/edição
+            )}
+        </div>
     );
 };
 
-export default UnidadeList;
+export default UnidadeListConsolidada;
