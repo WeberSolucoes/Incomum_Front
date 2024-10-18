181cac065276aac7a50383bacf9c400444705f1e
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-10-18 17:07:43 -0300

    Update Relatorio.tsx

--- src/components/specific/Relatorio.tsx
+++ src/components/specific/Relatorio.tsx
@@ -252,6 +252,32 @@ const Relatorio = () => {
     const scrollToBottom = () => {
         window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
     };
+    const handleExport = async () => {
+        try {
+            const response = await axios.get('http://18.118.35.25:8443/api/incomum/relatorio/download-relatorio/', {
+                params: {
+                    dataInicio: dateStart?.toISOString().split('T')[0], // Adicionei a formatação
+                    dataFim: dateEnd?.toISOString().split('T')[0],
+                    unidade: selectedUnidade, // Envia apenas a unidade selecionada
+                    areaComercial: selectedAreaComercial,
+                    agencia: selectedAgencia,
+                    vendedor: selectedVendedor,
+                },
+                responseType: 'blob',
+            });
+
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', 'relatorio.xlsx');
+            document.body.appendChild(link);
+            link.click();
+            link.remove();
+        } catch (error) {
+            console.error('Erro ao exportar Excel:', error);
+            toastError('Erro ao exportar Excel');
+        }
+    };
 
     return (
         <>
