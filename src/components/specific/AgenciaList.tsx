1ffc00e2b83bd253625dfcceab64fe22a7c12fe2
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-10-22 10:31:36 -0300

    Update AgenciaList.tsx

--- src/components/specific/AgenciaList.tsx
+++ src/components/specific/AgenciaList.tsx
@@ -12,42 +12,41 @@ const AgenciaList: React.FC = () => {
     const [items, setItems] = useState<AgenciaListResponse[]>([]);
     const [originalItems, setOriginalItems] = useState<AgenciaListResponse[]>([]);
     const [searchTerm, setSearchTerm] = useState('');
-    const [view, setView] = useState<'list' | 'create'>('list'); // Estado para controlar a visualização atual
+    const [view, setView] = useState<'list' | 'create'>('list');
+    const [loading, setLoading] = useState(false); // Estado de carregamento
 
-    const { setCodigo } = useCodigo(); // Acesso ao contexto
+    const { setCodigo } = useCodigo();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await apiGetAgencia();
-                const mappedData: AgenciaListResponse[] = response.data.map((item: any) => ({
-                    codigo: item.age_codigo,
-                    descricao: item.age_descricao,
-                    responsavel: item.age_responsavel,
-                    email: item.age_email,
-                }));
-                setItems(mappedData);
-                setOriginalItems(mappedData); // Armazena os dados originais
-            } catch (error) {
-                toastError('Erro ao buscar as agências');
-            }
-        };
+    const handleSearch = async () => {
+        if (searchTerm.length < 3) {
+            toastError('Por favor, insira pelo menos 3 caracteres para realizar a pesquisa.');
+            return; // Não realiza a pesquisa se a condição não for atendida
+        }
 
-        fetchData();
-    }, []);
+        setLoading(true); // Ativa o estado de carregamento
 
-    const handleSearch = () => {
-        if (searchTerm) {
+        try {
+            const response = await apiGetAgencia();
+            const mappedData: AgenciaListResponse[] = response.data.map((item: any) => ({
+                codigo: item.age_codigo,
+                descricao: item.age_descricao,
+                responsavel: item.age_responsavel,
+                email: item.age_email,
+            }));
+            setOriginalItems(mappedData); // Armazena os dados originais
+            
             const searchTermLower = searchTerm.toLowerCase();
-            const filteredItems = originalItems.filter(item =>
+            const filteredItems = mappedData.filter(item =>
                 item.descricao.toLowerCase().includes(searchTermLower) ||
                 item.codigo.toString().toLowerCase().includes(searchTermLower) ||
                 (item.responsavel && item.responsavel.toLowerCase().includes(searchTermLower)) ||
-                (item.email && item.email.toString().toLowerCase().includes(searchTermLower))
+                (item.email && item.email.toLowerCase().includes(searchTermLower))
             );
             setItems(filteredItems);
-        } else {
-            setItems(originalItems); // Restaura a lista original se não houver termo de pesquisa
+        } catch (error) {
+            toastError('Erro ao buscar as agências');
+        } finally {
+            setLoading(false); // Desativa o estado de carregamento
         }
     };
 
@@ -60,6 +59,7 @@ const AgenciaList: React.FC = () => {
         setCodigo(null); // Resetando o código para criar uma nova agência
         setView('create'); // Muda para a visualização de criação
     };
+
 
     const handleBackClick = () => {
         setView('list'); // Volta para a visualização da lista
@@ -81,11 +81,12 @@ const AgenciaList: React.FC = () => {
                             value={searchTerm}
                             onChange={(e) => setSearchTerm(e.target.value)} // Atualiza o termo de busca
                         />
-                        <Button
-                            label="Consultar"
-                            icon="pi pi-search"
+                       <Button
+                            label={loading ? 'Carregando...' : 'Consultar'} // Muda o texto durante o carregamento
+                            icon={loading ? 'pi pi-spin pi-spinner' : 'pi pi-search'} // Ícone de carregamento ou de busca
                             style={{ marginLeft: '10px', backgroundColor: '#0152a1' }}
                             onClick={handleSearch} // Chama a pesquisa ao clicar no botão
+                            disabled={loading} // Desabilita o botão enquanto está carregando
                         />
                         <Button
                             label="Criar"
