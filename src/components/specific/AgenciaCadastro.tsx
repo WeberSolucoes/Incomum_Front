5392ab49c940b92b6b2c580496e00c4dd635d5c1
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-11-06 17:22:42 -0300

    Update AgenciaCadastro.tsx

--- src/components/specific/AgenciaCadastro.tsx
+++ src/components/specific/AgenciaCadastro.tsx
@@ -163,7 +163,6 @@ const Agencia: React.FC<AgenciaCadastroProps> = ({onBackClick,onCodigoUpdate}) =
           setLoading(true);
           try {
               await apiDeleteAgencia(request.age_codigo);
-              form.reset();
               toastSucess('Cadastro excluído com sucesso.');
           } catch (error) {
               console.error('Erro ao excluir o cadastro:', error);
