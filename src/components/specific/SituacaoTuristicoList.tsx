ef810f3885afd702cca175c0235c2dac6a9075fa
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-11-22 16:32:34 -0300

    Update SituacaoTuristicoList.tsx

--- src/components/specific/SituacaoTuristicoList.tsx
+++ src/components/specific/SituacaoTuristicoList.tsx
@@ -7,6 +7,7 @@ import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 import { useCodigo } from '../../contexts/CodigoProvider'; // Importa o contexto
 import SituacaoTuristico from './SituacaoTuristico';
+import useEnterKey from '../../hooks/useEnterKey';
 
 const SituacaoTuristicoList: React.FC = () => {
     const [items, setItems] = useState<UnidadesListResponse[]>([]);
@@ -47,6 +48,8 @@ const SituacaoTuristicoList: React.FC = () => {
             setLoading(false); // Desativa o estado de carregamento
         }
     };
+
+    useEnterKey(handleSearch);
 
     const handleCodeClick = (codigo: number) => {
         setCodigo(codigo);
