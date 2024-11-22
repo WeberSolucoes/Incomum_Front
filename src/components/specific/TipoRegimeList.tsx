f8dff844ad9c22d463170bd71f6c75b9a808eb90
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-11-22 16:34:45 -0300

    Update TipoRegimeList.tsx

--- src/components/specific/TipoRegimeList.tsx
+++ src/components/specific/TipoRegimeList.tsx
@@ -8,6 +8,7 @@ import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 import { useCodigo } from '../../contexts/CodigoProvider'; // Importa o contexto
 import TipoRegime from "./TipoRegime";
+import useEnterKey from '../../hooks/useEnterKey';
 
 const TipoRegimeList: React.FC = () => {
     const [items, setItems] = useState<UnidadesListResponse[]>([]);
@@ -48,6 +49,8 @@ const TipoRegimeList: React.FC = () => {
             setLoading(false); // Desativa o estado de carregamento
         }
     };
+
+    useEnterKey(handleSearch);
 
     const handleCodeClick = (codigo: number) => {
         setCodigo(codigo);
