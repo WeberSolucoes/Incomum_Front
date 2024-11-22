9715466b9d7b6881aa1c8d54ff23d27566960b03
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-11-22 14:45:19 -0300

    Create SituacaoTuristico.tsx

---
+++ src/components/specific/SituacaoTuristico.tsx
@@ -0,0 +1,245 @@
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
+import { toast } from 'react-toastify';
+import { toastError, toastSucess } from "../../utils/customToast";
+import { useCodigo } from "../../contexts/CodigoProvider";
+import { CidadeCreateRequest, DepartamentoCreateRequest, SituacaoTuristicoCreateRequest } from "../../utils/apiObjects";
+import { apiCreateSituacaoTuristico, apiDeleteSituacaoTuristico,apiGetSituacaoTuristicoId, apiUpdateSituacaoTuristico } from "../../services/Api";
+import { ConfirmDialog, confirmDialog } from "primereact/confirmdialog";
+import { cpf } from 'cpf-cnpj-validator';
+import { Button } from "primereact/button";
+
+
+const SituacaoTuristico: React.FC = ({ onBackClick }) => {
+    const { codigo } = useCodigo(); // Assumindo que useCodigo fornece o código da unidade
+    const [request, setRequest] = useState<SituacaoTuristicoCreateRequest>({} as SituacaoTuristicoCreateRequest);
+    const [rua, setRua] = useState('');
+    const [numero, setNumero] = useState('');
+    const [cidade, setCidade] = useState('');
+    const [ibge, setibge] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [areacomercial, setAreaComercial] = useState('');
+    const [areasComerciais, setAreasComerciais] = useState<{ label: string, value: number }[]>([]);
+    const [selectedAreas, setSelectedAreas] = useState<number[]>([]);
+    const [checked, setChecked] = useState(false);
+    const [stu_codigo, setVenCodigo] = useState<number | null>(null); // Inicialmente nulo ou 
+    const [cpfValido, setCpfValido] = useState<boolean | null>(null);
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (!codigo) return;
+            try {
+                const response = await apiGetSituacaoTuristicoId(codigo);
+                const unidade = response.data;
+                setRequest(unidade);
+                setVenCodigo(unidade.stu_codigo); // Define o ID do vendedor
+                
+                if (unidade.loj_endereco) {
+                    const enderecoParts = unidade.loj_endereco.split(",");
+                    setRua(enderecoParts[0] || '');
+                    setNumero(enderecoParts[1] || '');
+                } else {
+                    setRua('');
+                    setNumero('');
+                }
+
+                setCidade(unidade.cid_codigo || '');
+                if (Array.isArray(unidade.areasComerciais)) {
+                    setSelectedAreas(unidade.areasComerciais.map((area: any) => area.aco_codigo));
+                } else {
+                    setSelectedAreas([]);
+                }
+                setChecked(unidade.loj_situacao === 1);
+
+                const responseCidade = await axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/municipios/${unidade.cid_codigo}`);
+                setCidade(responseCidade.data.nome || '');
+            } catch (error) {
+                console.error("Erro ao buscar dados:", error);
+                toastError("Erro ao buscar dados do Cidade.");
+            }
+        };
+        fetchData();
+    }, [codigo]);
+
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { id, value } = e.target;
+        setRequest(prevState => ({ ...prevState, [id]: value }));
+
+        if (id === 'ven_cpf') {
+            setCpfValido(cpf.isValid(value.replace(/\D/g, '')));
+        }
+    };
+
+    const handleDeleteClick = () => {
+        if (stu_codigo !== null && !showModal) { // Verifica se o modal não está aberto
+            setShowModal(true); // Abre o modal
+            confirmDialog({
+                message: 'Tem certeza de que deseja excluir este cadastro?',
+                header: 'Confirmar Exclusão',
+                icon: 'pi pi-exclamation-triangle',
+                accept: handleConfirmDelete,
+                reject: () => {
+                    setShowModal(false); // Fecha o modal se a exclusão for cancelada
+                    console.log('Exclusão cancelada');
+                },
+                acceptLabel: 'Sim, desejo excluir',
+                rejectLabel: 'Cancelar',
+                className: 'custom-confirm-dialog',
+            });
+        }
+    };
+
+    const handleConfirmDelete = async () => {
+        if (stu_codigo !== null) {
+            setLoading(true);
+            try {
+                await apiDeleteSituacaoTuristico(stu_codigo);
+                toast.success('Cadastro excluído com sucesso.');
+
+                // Limpa os campos do formulário após exclusão
+                handleReset();
+            } catch (error) {
+                toastError('Erro ao excluir o cadastro.');
+                console.error('Erro ao excluir o cadastro:', error);
+            } finally {
+                setLoading(false);
+            }
+        }
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setLoading(true);
+    
+        if (!request.stu_descricao) {
+            toastError("O campo Situação é obrigatório.");
+            setLoading(false);
+            return;
+        }
+    
+        try {
+            let response;
+            if (request.stu_codigo) {
+                response = await apiUpdateSituacaoTuristico(request.stu_codigo, request);
+            } else {
+                const { stu_codigo, ...newRequest } = request;
+                response = await apiCreateSituacaoTuristico(newRequest);
+            }
+    
+            if (response.status === 200 || response.status === 201) {
+                toastSucess("Situção Turistico salva com sucesso");
+    
+                // Atualize o `cid_codigo` no estado após criação bem-sucedida
+                if (!request.stu_codigo && response.data && response.data.stu_codigo) {
+                    setRequest(prev => ({
+                        ...prev,
+                        stu_codigo: response.data.stu_codigo
+                    }));
+                    setVenCodigo(response.data.stu_codigo); // Atualize também o estado `cid_codigo`
+                }
+            } else {
+                toastError("Erro ao salvar a Situação Turistica");
+            }
+        } catch (error: any) {
+            console.error("Erro:", error);
+            if (error.response) {
+                const status = error.response.status;
+                const data = error.response.data;
+                if (status === 400) {
+                    toastError("Dados inválidos. Verifique os campos e tente novamente.");
+                } else if (status === 401) {
+                    toastError("Não autorizado. Verifique suas credenciais.");
+                } else if (status === 500) {
+                    toastError("Erro interno do servidor. Tente novamente mais tarde.");
+                } else {
+                    toastError(`Erro desconhecido: ${data.detail || "Verifique os campos e tente novamente"}`);
+                }
+            } else {
+                toastError("Erro de conexão. Verifique sua rede e tente novamente.");
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleReset = () => {
+        setRequest({} as SituacaoTuristicoCreateRequest);
+        setSelectedAreas([]);
+        setRua('');
+        setNumero('');
+        setCidade('');
+        setChecked(false);
+    };
+
+
+    return (
+        <form className="erp-form" onSubmit={handleSubmit}>
+
+            <div className="form-row">
+                <div className="form-group">
+                    <label htmlFor="dep_codigo">Codigo</label>
+                    <input
+                        type="text"
+                        id="dep_codigo"
+                        name="dep_codigo"
+                        value={request.stu_codigo || ''}
+                        onChange={handleInputChange}
+                        style={{width:'200px'}}
+                        disabled
+                    />
+                </div>
+            </div>
+
+            {/* Segunda linha */}
+            <div className="form-row">
+                <div className="form-group">
+                    <label htmlFor="stu_descricao">Situação Turistico</label>
+                    <input
+                        type="text"
+                        id="stu_descricao"
+                        name="stu_descricao"
+                        value={request.stu_descricao || ''}
+                        onChange={handleInputChange}
+                        style={{width:'100%'}}
+                    />
+                </div>
+            </div>
+
+
+            <div className="form-row">
+                    <Button
+                        label="Voltar"
+                        icon="pi pi-arrow-left"
+                        style={{backgroundColor: '#0152a1',width:'100px',height:'34px',marginLeft:'580px',borderRadius:'4px' }}
+                        onClick={onBackClick} // Chama a função passada como prop
+                    />
+                {/* Condição para renderizar o botão de exclusão */}
+                {request.stu_codigo && (
+                <button
+                    style={{marginLeft:'0px',color:'white',width:'100px'}}
+                    type="button"
+                    className="reset-btn"
+                    onClick={handleDeleteClick}
+                    disabled={loading}
+                >
+                    <i className="fas fa-trash-alt"></i>{loading ? "Excluindo..." : "Excluir"}
+                </button>
+                )}
+                
+                <button
+                    style={{color:'white',backgroundColor:'#0152a1',marginLeft: request.stu_codigo ? '14px' : '0px',display: request.stu_codigo ? 'none' :''}}
+                    type="button"
+                    className="reset-btn"
+                    onClick={handleReset}
+                >
+                    <i className="fas fa-trash-alt"></i> Limpar
+                </button>
+                <button style={{width:'100px',height:'34px',padding:'inherit'}} disabled={loading} type="submit" className="submit-btn"><i style={{marginRight:'10px'}}className="fas fa-save"></i>{loading ? 'Salvando...' : 'Salvar'}</button>
+            </div>
+        </form>
+    );
+};
+
+export default SituacaoTuristico;
