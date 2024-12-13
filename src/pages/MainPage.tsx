7fc36605dd535ce060bc90fa0a0a08143b724c9a
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-13 17:22:39 -0300

    Update MainPage.tsx

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -13,7 +13,7 @@ import AreaComercialCadastro from '../components/specific/AreaComercialCadastro'
 import { useAuth } from '../contexts/AuthProvider';
 import { useNavigate } from 'react-router-dom';
 import { useCodigo } from '../contexts/CodigoProvider';
-import '../assets/styles/pages/Agencia.css?v=129';
+import '../assets/styles/pages/Agencia.css?v=130';
 import '../assets/styles/pages/sidebar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
