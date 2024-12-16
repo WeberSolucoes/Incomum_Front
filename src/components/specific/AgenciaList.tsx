4a1ee9411830df8ea45415861cecc22d9759dba8
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-16 14:10:00 -0300

    Update AgenciaList.tsx

--- src/components/specific/AgenciaList.tsx
+++ src/components/specific/AgenciaList.tsx
@@ -112,7 +112,7 @@ const AgenciaList: React.FC = () => {
                             style={{ width: '300px' }}
                             placeholder="Buscar"
                             value={searchTerm}
-                            onChange={(e) => setSearchTerm(e.target.value)}
+                            onChange={(e) => setSearchTerm(e.target.value.toUpperCase())}
                         />
                         <Button
                             label={loading ? 'Carregando...' : 'Consultar'}
