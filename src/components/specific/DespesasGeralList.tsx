fb94dcdb4e9dbe5851adee59cc95689c99b677a3
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-16 17:46:24 -0300

    Update DespesasGeralList.tsx

--- src/components/specific/DespesasGeralList.tsx
+++ src/components/specific/DespesasGeralList.tsx
@@ -46,7 +46,7 @@ const DespesasGeralList: React.FC = () => {
             );
             setItems(filteredItems);
         } catch (error) {
-            toastError('Erro ao buscar a Cidade');
+            toastError('Erro ao buscar Despesas Geral');
         } finally {
             setLoading(false); // Desativa o estado de carregamento
         }
