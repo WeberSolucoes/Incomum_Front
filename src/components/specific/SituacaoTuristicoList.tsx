72184ea564501533e84b1567cc84356f8202fa97
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-16 16:26:22 -0300

    Update SituacaoTuristicoList.tsx

--- src/components/specific/SituacaoTuristicoList.tsx
+++ src/components/specific/SituacaoTuristicoList.tsx
@@ -81,7 +81,7 @@ const SituacaoTuristicoList: React.FC = () => {
                             style={{ width: '300px' }}
                             placeholder="Buscar"
                             value={searchTerm}
-                            onChange={(e) => setSearchTerm(e.target.value)}
+                            onChange={(e) => setSearchTerm(e.target.value.toUpperCase())}
                         />
                         <Button
                             label={loading ? 'Carregando...' : 'Consultar'}
