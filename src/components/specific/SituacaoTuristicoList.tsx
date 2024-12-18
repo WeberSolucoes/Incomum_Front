3decc656aa523767e4f07dcdcb7f74dcfd7bc1ce
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-18 15:52:39 -0300

    Update SituacaoTuristicoList.tsx

--- src/components/specific/SituacaoTuristicoList.tsx
+++ src/components/specific/SituacaoTuristicoList.tsx
@@ -70,7 +70,7 @@ const SituacaoTuristicoList: React.FC = () => {
     };
 
     const columns = [
-        { field: 'codigo', header: 'Codigo' },
+        { field: 'codigo', header: 'Codigo', style: { width: '6rem', textAlign: 'left' } },
         { field: 'descricao', header: 'Situação Turistico' },
     ];
 
