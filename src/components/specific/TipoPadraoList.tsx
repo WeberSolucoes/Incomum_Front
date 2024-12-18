f8c5a7c9590433c1ee50453279fbe6f5debde3a2
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-18 15:54:18 -0300

    Update TipoPadraoList.tsx

--- src/components/specific/TipoPadraoList.tsx
+++ src/components/specific/TipoPadraoList.tsx
@@ -71,7 +71,7 @@ const TipoPadraoList: React.FC = () => {
     };
 
     const columns = [
-        { field: 'codigo', header: 'Codigo' },
+        { field: 'codigo', header: 'Codigo', style: { width: '6rem', textAlign: 'left' } },
         { field: 'descricao', header: 'Tipo Padrão' },
     ];
 
