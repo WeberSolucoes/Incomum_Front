46642092eb12dedf71c6f1d2c51ef894497a5d29
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-18 15:46:48 -0300

    Update CepList.tsx

--- src/components/specific/CepList.tsx
+++ src/components/specific/CepList.tsx
@@ -73,7 +73,7 @@ const CepList: React.FC = () => {
     };
 
     const columns = [
-        { field: 'codigo', header: 'Codigo' },
+        { field: 'codigo', header: 'Codigo', style: { width: '6rem', textAlign: 'left' } },
         { field: 'descricao', header: 'Bairro' },
         { field: 'logradouro', header: 'Logradouro' },
     ];
