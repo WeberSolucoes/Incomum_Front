d336a3335cca1e0da3992fdb38014ce073404346
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-18 15:42:32 -0300

    Update AgenciaList.tsx

--- src/components/specific/AgenciaList.tsx
+++ src/components/specific/AgenciaList.tsx
@@ -110,7 +110,7 @@ const AgenciaList: React.FC = () => {
     };
 
     const columns = [
-        { field: 'codigo', header: 'Codigo', style: { width: '6rem', textAlign: 'center' }},
+        { field: 'codigo', header: 'Codigo', style: { width: '6rem', textAlign: 'left' }},
         { field: 'descricao', header: 'Descrição' },
         { field: 'cnpj', header: 'Cnpj' },
         { field: 'razaosocial', header: 'Razão Social' }
