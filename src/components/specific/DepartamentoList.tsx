3e9c646a28d5f37ca93473a17228022aa45d3217
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-18 10:01:04 -0300

    Update DepartamentoList.tsx

--- src/components/specific/DepartamentoList.tsx
+++ src/components/specific/DepartamentoList.tsx
@@ -70,6 +70,11 @@ const DepartamentoList: React.FC = () => {
         });
     };
 
+    const columns = [
+        { field: 'codigo', header: 'Codigo' },
+        { field: 'descricao', header: 'Departamento' },
+    ];
+
     const paisDescricao = codigo ? items.find(item => item.codigo === codigo)?.descricao : '';
 
     return (
@@ -101,7 +106,8 @@ const DepartamentoList: React.FC = () => {
                     <GenericTable 
                         filteredItems={items} 
                         emptyMessage="Nenhum Departamento encontrado" 
-                        onCodeClick={handleCodeClick} 
+                        onCodeClick={handleCodeClick}
+                        columns={columns}
                     />
                 </>
             ) : (
