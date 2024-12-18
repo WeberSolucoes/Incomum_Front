a374c5cf1f136dfd3e774af83193d74d0033619a
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2024-12-18 10:06:11 -0300

    Update FormaPagamentoList.tsx

--- src/components/specific/FormaPagamentoList.tsx
+++ src/components/specific/FormaPagamentoList.tsx
@@ -69,6 +69,11 @@ const FormaPagamentoList: React.FC = () => {
         });
     };
 
+    const columns = [
+        { field: 'codigo', header: 'Codigo' },
+        { field: 'descricao', header: 'Forma Pagamento' },
+    ];
+
     const paisDescricao = codigo ? items.find(item => item.codigo === codigo)?.descricao : '';
 
     return (
@@ -100,7 +105,8 @@ const FormaPagamentoList: React.FC = () => {
                     <GenericTable 
                         filteredItems={items} 
                         emptyMessage="Nenhuma Forma De Pagamento encontrada" 
-                        onCodeClick={handleCodeClick} 
+                        onCodeClick={handleCodeClick}
+                        columns={columns}
                     />
                 </>
             ) : (
