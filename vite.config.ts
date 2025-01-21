bbe705f82971e2e191a1ca524b3d351385c5bd4d
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-01-21 16:37:23 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 80
-  }
+    host: '0.0.0.0', // Permite acesso remoto
+    port: 80,        // Porta do servidor
+  },
+  optimizeDeps: {
+    exclude: ['.git/**'], // Exclui a pasta .git e outros arquivos desnecessários
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // Configuração para evitar inclusões desnecessárias no build final
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
 
