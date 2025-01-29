4c4c6271708fa062963697b7699c9ffe2c1d234a
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-01-29 16:40:38 -0300

    Update AgenciaCadastro.tsx

--- src/components/specific/AgenciaCadastro.tsx
+++ src/components/specific/AgenciaCadastro.tsx
@@ -8,6 +8,7 @@ import { apiDeleteAgencia, apiGetAgenciaById, apiGetArea, apiPostCreateAgencia, 
 import { ConfirmDialog, confirmDialog } from "primereact/confirmdialog";
 import { cnpj } from "cpf-cnpj-validator";
 import { Button } from 'primereact/button';
+import Select from 'react-select';
 
 interface AgenciaCadastroProps {
     onCodigoUpdate: (codigo: number) => void; // Define a prop como uma função que recebe um número
@@ -27,6 +28,8 @@ const Agencia: React.FC<AgenciaCadastroProps> = ({onBackClick,onCodigoUpdate}) =
   const [checked, setChecked] = useState(false);
   const [cnpjValido, setCnpjValido] = useState<boolean | null>(null);
   const [error, setError] = useState({});
+  const [searchTerm, setSearchTerm] = useState('');
+  const [cidades, setCidades] = useState<{ label: string, value: number }[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -178,6 +181,50 @@ const Agencia: React.FC<AgenciaCadastroProps> = ({onBackClick,onCodigoUpdate}) =
           }
       }
   };
+
+
+  const fetchUnidades = async (search: string) => {
+      if (search.length < 3) {
+          setCidades([]); // Limpa o estado se o termo de pesquisa for muito curto
+          return;
+      }
+
+      try {
+          const response = await axios.get(
+              `http://127.0.0.1:8000/api/incomum/cidade/search/`,
+              { params: { q: search } }
+          );
+          const data = response.data;
+
+          console.log("Dados crus da API:", data); // Verificar dados crus
+
+          if (Array.isArray(data) && data.length > 0) {
+              setCidades(data); // Atualiza somente se houver resultados
+              console.log("Estado atualizado com cidades:", data);
+          } else {
+              console.log("Nenhum dado encontrado");
+              setCidades([]);
+          }
+      } catch (error) {
+          console.error("Erro ao buscar cidades:", error);
+          setCidades([]); // Limpa em caso de erro
+      } finally {
+          setLoading(false);
+      }
+  }; 
+
+  useEffect(() => {
+      console.log("Cidades após a atualização:", cidades); // Verifique o estado
+  }, [cidades]); 
+
+    // Efetua a busca quando o valor do termo de busca mudar
+  useEffect(() => {
+      if (searchTerm.length >= 3) {
+        fetchUnidades(searchTerm); // Realiza a consulta para termos com 3 ou mais caracteres
+      } else {
+        setCidades([]); // Limpa as cidades se o termo for menor que 3
+      }
+  }, [searchTerm]); // A busca será chamada sempre que `searchTerm` mudar  
   
   const campoMapeamento: Record<keyof AgenciaCreateRequest, string> = {
     age_numero: 'Número',
@@ -367,6 +414,17 @@ const Agencia: React.FC<AgenciaCadastroProps> = ({onBackClick,onCodigoUpdate}) =
         }
     }
   };
+
+
+  const handleCidadeChange = (selectedOption: { label: string; value: number } | null) => {
+    if (selectedOption) {
+        console.log("Cidade selecionada:", selectedOption);
+        setibge(selectedOption.value); // Atualiza o estado com o valor selecionado
+    } else {
+        setibge(null); // Reseta o valor se nada for selecionado
+    }
+  };
+    
   return (
     <form className="erp-form" onSubmit={handleSubmit}>
       <div className="form-row">
@@ -444,9 +502,24 @@ const Agencia: React.FC<AgenciaCadastroProps> = ({onBackClick,onCodigoUpdate}) =
           <input type="text" id="age_bairro" name="age_bairro" value={request.age_bairro || ''}  onChange={(e) => handleInputChange(e, e.target.value.toUpperCase())} />
         </div>
         <div className="form-group">
-          <label htmlFor="age_cidade">Cidade</label>
-          <input type="text" id="cid_codigo" name="cid_codigo" value={cidade || ''}  onChange={(e) => handleInputChange(e, e.target.value.toUpperCase())} />
-        </div>
+          <label htmlFor="cid_codigo">Cidade</label>
+            <Select
+              id="cid_codigo"
+              name="cid_codigo"
+              isClearable
+              isLoading={loading} // Indicador de carregamento
+              options={cidades} // Estado cidades atualizado com os dados crus da API
+              onInputChange={(inputValue, { action }) => {
+                if (action === "input-change") {
+                  setSearchTerm(inputValue); // Atualiza o termo de pesquisa
+                  fetchUnidades(inputValue); // Faz a chamada à API
+                }
+              }}
+              onChange={handleCidadeChange} // Lida com a mudança de valor selecionado
+              value={cidades.find((option) => option.value === ibge) || null} // Define o valor atual
+              placeholder="Selecione uma Cidade"
+            />
+          </div>
       </div>
 
 
