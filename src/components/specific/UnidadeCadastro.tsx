e9d87973c1f1f1d2d1958b4f16a1e388348cac7a
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-01-31 09:47:41 -0300

    Update UnidadeCadastro.tsx

--- src/components/specific/UnidadeCadastro.tsx
+++ src/components/specific/UnidadeCadastro.tsx
@@ -522,7 +522,7 @@ const Unidade: React.FC = ({onBackClick}) => {
                     options={cidades}
                     onInputChange={(inputValue, { action }) => {
                         if (action === "input-change" && inputValue.length >= 3) {
-                        fetchUnidades(inputValue);
+                        fetchUnidades(inputValue.toUpperCase());
                         } else if (inputValue.length < 3) {
                         setCidades([]);
                         }
