7fe8945772d1b3204382fd9a0e352f879864e6fc
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-01-31 13:43:41 -0300

    Update Cep.tsx

--- src/components/specific/Cep.tsx
+++ src/components/specific/Cep.tsx
@@ -327,10 +327,9 @@ const Cep: React.FC = ({ onBackClick }) => {
                     isLoading={loading}
                     options={cidades}
                     onInputChange={(inputValue, { action }) => {
-                        if (action === "input-change" && inputValue.length >= 3) {
-                        fetchUnidades(inputValue.toUpperCase());
-                        } else if (inputValue.length < 3) {
-                        setCidades([]);
+                        if (action === "input-change") {
+                            setSearchTerm(inputValue); // Atualiza o termo de pesquisa
+                            fetchUnidades(inputValue.toUpperCase()); // Faz a chamada à API
                         }
                     }}
                     onChange={handleCidadeChange}
