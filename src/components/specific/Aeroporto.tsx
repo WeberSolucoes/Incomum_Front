1643f4f2a8fcda8ca399f7dd21a04fe11d8bdad5
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-01-31 17:30:22 -0300

    Update Aeroporto.tsx

--- src/components/specific/Aeroporto.tsx
+++ src/components/specific/Aeroporto.tsx
@@ -29,7 +29,6 @@ const Aeroporto: React.FC = ({ onBackClick }) => {
     const [aer_codigo, setVenCodigo] = useState<number | null>(null);
     const [searchTerm, setSearchTerm] = useState('');
     const dispatch = useDispatch();
-    const [tabs, setTabs] = useState([]);
 
 
     useEffect(() => {
@@ -262,17 +261,9 @@ const Aeroporto: React.FC = ({ onBackClick }) => {
                     type="button"
                     className="btn btn-link p-0 ml-1"
                     onClick={() => {
-                        // Verifica se a aba "Cidade" já existe
-                        const tabExists = tabs.some(tab => tab.key === 'Cidade');
-                    
-                        if (!tabExists) {
-                            // Adiciona a aba "Cidade" e troca para ela se não existir
-                            setTabs([...tabs, { key: 'Cidade', title: 'Cidade', state: {} }]);
-                            setActiveTab('Cidade'); // Troca para a aba Cidade
-                        } else {
-                            // Apenas troca para a aba "Cidade" se ela já existir
-                            setActiveTab('Cidade');
-                        }
+                        // Adiciona a aba Cidade e troca para ela
+                        dispatch(setActiveTab('Cidade')); // Troca para a aba Cidade
+                        dispatch(addTab({ key: 'Cidade', title: 'Cidade', state: {} })); // Adiciona a aba no Redux
                     }}
                     style={{
                         fontSize: "1.5rem",
