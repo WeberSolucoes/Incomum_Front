8cd924c774f0ecd5f6172e63e2b096e0a0e89e81
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-01-31 09:45:04 -0300

    Update AgenciaCadastro.tsx

--- src/components/specific/AgenciaCadastro.tsx
+++ src/components/specific/AgenciaCadastro.tsx
@@ -538,7 +538,7 @@ const Agencia: React.FC<AgenciaCadastroProps> = ({onBackClick,onCodigoUpdate}) =
                 options={cidades}
                 onInputChange={(inputValue, { action }) => {
                     if (action === "input-change" && inputValue.length >= 3) {
-                    fetchUnidades(inputValue);
+                    fetchUnidades(inputValue.toUpperCase());
                     } else if (inputValue.length < 3) {
                     setCidades([]);
                     }
