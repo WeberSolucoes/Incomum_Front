529da61241edbe72f2ff7a1dd63e65d2b5323fbd
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-01-31 17:31:00 -0300

    Update AgenciaCadastro.tsx

--- src/components/specific/AgenciaCadastro.tsx
+++ src/components/specific/AgenciaCadastro.tsx
@@ -34,7 +34,6 @@ const Agencia: React.FC<AgenciaCadastroProps> = ({onBackClick,onCodigoUpdate}) =
   const [searchTerm, setSearchTerm] = useState('');
   const [cidades, setCidades] = useState<{ label: string, value: number }[]>([]);
   const dispatch = useDispatch();
-  const [tabs, setTabs] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -515,18 +514,10 @@ const Agencia: React.FC<AgenciaCadastroProps> = ({onBackClick,onCodigoUpdate}) =
                 type="button"
                 className="btn btn-link p-0 ml-1"
                 onClick={() => {
-                    // Verifica se a aba "Cidade" já existe
-                    const tabExists = tabs.some(tab => tab.key === 'Cidade');
-                
-                    if (!tabExists) {
-                        // Adiciona a aba "Cidade" e troca para ela se não existir
-                        setTabs([...tabs, { key: 'Cidade', title: 'Cidade', state: {} }]);
-                        setActiveTab('Cidade'); // Troca para a aba Cidade
-                    } else {
-                        // Apenas troca para a aba "Cidade" se ela já existir
-                        setActiveTab('Cidade');
-                    }
-                }}
+                        // Adiciona a aba Cidade e troca para ela
+                        dispatch(setActiveTab('Cidade')); // Troca para a aba Cidade
+                        dispatch(addTab({ key: 'Cidade', title: 'Cidade', state: {} })); // Adiciona a aba no Redux
+                    }}
                 style={{
                     fontSize: "1.5rem",
                     color: "#007bff",
