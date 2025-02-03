10b1504ffbe05743a971b4809613ee16ced0af2b
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-03 16:31:32 -0300

    Update Cep.tsx

--- src/components/specific/Cep.tsx
+++ src/components/specific/Cep.tsx
@@ -323,7 +323,7 @@ const Cep: React.FC = ({ onBackClick }) => {
                     <label htmlFor="cid_codigo" style={{ whiteSpace: "nowrap" }}>Cidade</label>
                     <IconButton 
                         onClick={handleClick} 
-                        sx={{ color: "#0152a1", backgroundColor: "white", padding: "5px",height:'34px', }}
+                        sx={{ color: "#0152a1", backgroundColor: "transparent", padding: "5px",height:'30px', }}
                     >
                         <AddToPhotosIcon sx={{ fontSize: 30 }} />
                     </IconButton>
