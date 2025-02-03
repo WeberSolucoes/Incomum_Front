3046daa505dcc7fe8b907f766ec170d9f6a64f8d
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-03 11:11:08 -0300

    Update VendedorCadastro.tsx

--- src/components/specific/VendedorCadastro.tsx
+++ src/components/specific/VendedorCadastro.tsx
@@ -328,6 +328,12 @@ const Vendedor: React.FC = ({onBackClick}) => {
         }
 
         dispatch(setActiveTab('Cidade')); // Troca para a aba "Cidade"
+
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth' // Deixa a rolagem suave
+        });
     };
   
 
