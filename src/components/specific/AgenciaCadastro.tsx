f787fdc9f8bbbe378f4e4a5dd22ed0694f5e59ee
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-03 16:30:59 -0300

    Update AgenciaCadastro.tsx

--- src/components/specific/AgenciaCadastro.tsx
+++ src/components/specific/AgenciaCadastro.tsx
@@ -536,7 +536,7 @@ const Agencia: React.FC<AgenciaCadastroProps> = ({onBackClick,onCodigoUpdate}) =
             <label htmlFor="cid_codigo" style={{ whiteSpace: "nowrap" }}>Cidade</label>
             <IconButton 
                 onClick={handleClick} 
-                sx={{ color: "#0152a1", backgroundColor: "white", padding: "5px",height:'34px', }}
+                sx={{ color: "#0152a1", backgroundColor: "transparent", padding: "5px",height:'30px', }}
             >
                 <AddToPhotosIcon sx={{ fontSize: 30 }} />
             </IconButton>
