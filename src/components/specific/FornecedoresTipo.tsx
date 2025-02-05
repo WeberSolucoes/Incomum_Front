f835a9e972fa1cba1350c74c0dd223ca890c4fbf
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-05 10:39:50 -0300

    Update FornecedoresTipo.tsx

--- src/components/specific/FornecedoresTipo.tsx
+++ src/components/specific/FornecedoresTipo.tsx
@@ -1,157 +1,311 @@
-import React, { useState } from 'react';
-import { AgenciaListResponse } from '../../utils/apiObjects';
-import GenericTable from '../common/GenericTable';
-import { apiGetParceiro } from '../../services/Api';
-import { toastError } from '../../utils/customToast';
-import { Button } from 'primereact/button';
-import { InputText } from 'primereact/inputtext';
-import { useCodigo } from '../../contexts/CodigoProvider'; // Importa o contexto
-import { TabPanel, TabView } from 'primereact/tabview';
-import ImageUpload from './logo';
-import Fornecedores from './Fornecedores';
-import FornecedoresContato from './FornecedoresContato';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from 'react-toastify';
+import { toastError, toastSucess } from "../../utils/customToast";
+import { useCodigo } from "../../contexts/CodigoProvider";
+import { FornecedorTipoCreateRequest, ParceiroContatoCreateRequest} from "../../utils/apiObjects";
+import {  apiCreateFornecedorTipo, apiDeleteFornecedorTipo, apiGetFornecedorTipo, apiUpdateFornecedorTipo } from "../../services/Api";
+import { ConfirmDialog, confirmDialog } from "primereact/confirmdialog";
+import { Button } from "primereact/button";
+import GenericTable from "../common/GenericTable";
+import { Dialog } from "primereact/dialog";
 
-interface ParceiroListProps {
-    parceiroId: number | null; // Aceita null para representar uma nova agência
-}
 
-const FornecedoresTipo: React.FC<ParceiroListProps> = () => {
-    const { codigo,setCodigo } = useCodigo(); // Ajuste conforme a origem do código
-    const [items, setItems] = useState<AgenciaListResponse[]>([]);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [view, setView] = useState<'list' | 'create' | 'uploadImage'>('list'); // Adiciona 'uploadImage'
-    const [loading, setLoading] = useState(false);
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [parceiroId, setAgenciaId] = useState<number | null>(null); // Estado para ID da agência
-    const [isCadastroConcluido, setIsCadastroConcluido] = useState(false);
+const ForncedoresTipo: React.FC = (onBackClick) => {
+    const [request, setRequest] = useState<FornecedorTipoCreateRequest>({} as FornecedorTipoCreateRequest);
+    const [agencias, setAgencias] = useState<{ id: number; nome: string }[]>([]); // Corrigido o nome do estado
+    const [modalVisible, setModalVisible] = useState(false);
+    const { codigo } = useCodigo(); // Presumo que esse hook retorna algum valor do código
+    const [editing, setEditing] = useState(false); // Estado para diferenciar entre criação e edição
+    const [agenteNome, setAgenteNome] = useState(''); // Nome do agente que está sendo editado
+    const [tpa_codigo, setVenCodigo] = useState<number | null>(null); // Código do parceir
+    const [filteredAgentes, setFilteredAgentes] = useState<FornecedorTipoCreateRequest[]>([]); // Filtra os agentes que devem ser exibidos
+    const [loading, setLoading] = useState(false); // Estado para indicar se há carregamento de dados
+    const [registroEditar, setRegistroEditar] = useState<FornecedorTipoCreateRequest | null>(null); // Para armazenar o agente a ser editado
+    const [selectedAgente, setSelectedAgente] = useState<number | null>(null);
 
-    const handleCadastroConcluido = () => {
-        setIsCadastroConcluido(true);
-    };
+    const agenteColumns = [
+        { field: "pco_codigo", header: "Código Contato" },
+        { field: "par_codigo", header: "Código Parceiro" },
+        { field: "pco_descricao", header: "Descrição" },
+        { field: "pco_observacao", header: "Observação" },
+        { field: "pco_fone", header: "Telefone" },
+    ];
 
-    const getTitle = () => {
-        switch (activeIndex) {
-            case 0:
-                return 'Cadastro Parceiro';
-            case 1:
-                return 'Cadastro Contato';
-            case 2:
-                return 'Logo Agência';
-            default:
-                return 'Cadastro Agência';
-        }
-    };
+    useEffect(() => {
+        console.log('Código da agência:', codigo);
+        console.log('Agentes filtrados:', filteredAgentes);
+        const parceiroId = codigo; // Exemplo de ID de agência
+    }, [codigo]);
 
-    const handleSearch = async () => {
-        if (searchTerm.length < 3) {
-            toastError('Por favor, insira pelo menos 3 caracteres para realizar a pesquisa.');
-            return;
-        }
-
-        setLoading(true);
-
-        try {
-            const response = await apiGetParceiro();
-            const mappedData: AgenciaListResponse[] = response.data.map((item: any) => ({
-                codigo: item.par_codigo,
-                descricao: item.par_descricao,
-                responsavel: item.par_obs,
-                email: item.par_cnpjcpf,
+    useEffect(() => {
+        if (codigo !== null) {
+            setRequest((prev) => ({
+                ...prev,
+                par_codigo: codigo, // Insere o código no request
+                tco_codigo: codigo,
             }));
-            setItems(mappedData.filter(item =>
-                item.descricao.toLowerCase().includes(searchTerm.toLowerCase())
-            ));
+        }
+    }, [codigo]);
+    
+
+    const fetchParceiroContatos = async () => {
+        setLoading(true);
+        try {
+            const response = await apiGetFornecedorTipo();
+            if (response.status === 200) {
+                // Mapeia os dados retornados da API para o formato esperado pela GenericTable
+                const data = Array.isArray(response.data) ? response.data : [];
+                const mappedData = data.map((item) => ({
+                    codigo: item.tpa_codigo, // Adapta para o campo esperado
+                    descricao: item.tpa_descricao,
+                }));
+                setFilteredAgentes(mappedData); // Atualiza o estado com os dados transformados
+            } else {
+                toastError("Erro ao carregar os dados.");
+            }
         } catch (error) {
-            toastError('Erro ao buscar os Parceiro');
+            console.error("Erro:", error);
+            toastError("Erro ao buscar os dados.");
         } finally {
             setLoading(false);
         }
     };
 
-    const handleCodeClick = (codigo: number) => {
-        setCodigo(codigo);
-        setView('create');
-        setActiveIndex(0);
+    
+    useEffect(() => {
+        fetchParceiroContatos();
+    }, []);
+
+
+    const handleDeleteClick = () => {
+        if (tpa_codigo !== null) { // Verifica se ven_codigo não é nulo
+            confirmDialog({
+                message: 'Tem certeza de que deseja excluir este cadastro?',
+                header: 'Confirmar Exclusão',
+                icon: 'pi pi-exclamation-triangle',
+                accept: handleConfirmDelete,
+                reject: () => console.log('Exclusão cancelada'),
+                acceptLabel: 'Sim, desejo excluir',
+                rejectLabel: 'Cancelar',
+                className: 'custom-confirm-dialog',
+            });
+        }
     };
 
-    const handleCreateClick = () => {
-        setAgenciaId(null);
-        setView('create');
-        setActiveIndex(0);
+    const handleConfirmDelete = async () => {
+        if (tpa_codigo !== null) { // Verifica se ven_codigo não é nulo
+            setLoading(true);
+            try {
+                await apiDeleteFornecedorTipo(tpa_codigo);
+                toast.success('Cadastro excluído com sucesso.');
+            } catch (error) {
+                toastError('Erro ao excluir o cadastro.');
+                console.error('Erro ao excluir o cadastro:', error);
+            } finally {
+                setLoading(false);
+            }
+        }
     };
 
-    const handleImageUploadClick = () => {
-        setView('uploadImage');
+    // Função para lidar com mudanças nos inputs
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        setRequest({ ...request, [name]: value });
+
     };
 
-    const handleBackClick = () => {
-        setView('list');
-        setActiveIndex(0);
+    // Função de submissão do formulário, diferenciando criação e edição
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+    
+        
+        setLoading(true);
+        try {
+    
+            let response;
+            if (request.tpa_codigo) {
+                // Atualizar vendedor existente
+                response = await apiUpdateFornecedorTipo(request.tpa_codigo, request);
+            } else {
+                // Criar novo vendedor
+                const { tpa_codigo, ...newRequest } = request; // Remove ven_codigo
+                response = await apiCreateFornecedorTipo(newRequest);
+            }
+    
+    
+            if (response.status === 200 || response.status === 201) {
+                toastSucess("Parceiro Contato salvo com sucesso");
+            } else {
+                toastError("Erro ao salvar o Parceiro Contato");
+            }
+        } catch (error: any) {
+            console.error("Erro:", error);
+            if (error.response) {
+                const status = error.response.status;
+                const data = error.response.data;
+                if (status === 400) {
+                    toastError("Dados inválidos. Verifique os campos e tente novamente.");
+                } else if (status === 401) {
+                    toastError("Não autorizado. Verifique suas credenciais.");
+                } else if (status === 500) {
+                    toastError("Erro interno do servidor. Tente novamente mais tarde.");
+                } else {
+                    toastError(`Erro desconhecido: ${data.detail || "Verifique os campos e tente novamente"}`);
+                }
+            } else {
+                toastError("Erro de conexão. Verifique sua rede e tente novamente.");
+            }
+        } finally {
+            setLoading(false);
+        }
     };
+
+    const handleReset = (e: React.FormEvent) => {
+        e.preventDefault();
+        setRequest({} as FornecedorTipoCreateRequest);
+    };
+
+    const closeDialog = () => setModalVisible(false);
+    
+    useEffect(() => {
+        if (modalVisible && editing) {
+            console.log('Dados atualizados para edição:', request);
+        }
+    }, [modalVisible, request]);
+    
+    const handleEditAgente = (codigo) => {
+        console.log("Lista de Agentes:", filteredAgentes);
+        console.log("Código recebido para edição:", codigo, "Tipo:", typeof codigo);
+    
+        filteredAgentes.forEach((agente, index) => {
+            console.log(`Agente ${index}:`, agente);
+            console.log(`Propriedades do Agente ${index}:`, Object.keys(agente));
+        });
+    
+        // Agora usamos 'codigo' ao invés de 'tpa_codigo'
+        const agenteParaEditar = filteredAgentes.find(agente => {
+            console.log("Comparando:", agente.codigo, "Tipo:", typeof agente.codigo);
+            return Number(agente.codigo) === Number(codigo);
+        });
+    
+        if (agenteParaEditar) {
+            console.log("✅ Agente encontrado:", agenteParaEditar);
+    
+            setRequest({
+                tpa_codigo: agenteParaEditar.codigo || '', // Ajustando para 'codigo'
+                tpa_descricao: agenteParaEditar.descricao || '', // Ajustando para 'descricao'
+            });
+            setAgenteNome(agenteParaEditar.descricao);
+            setSelectedAgente(codigo);
+            setEditing(true);
+            setModalVisible(true);
+        } else {
+            console.error('❌ Agente não encontrado para edição:', codigo);
+        }
+    };
+    
+    
+    
 
     return (
-        <div>
-            {view === 'list' ? (
-                <>
-                    <div style={{ display: 'flex', alignItems: 'center', marginBottom: '1rem',marginTop:'10px' }}>
-                        <InputText
-                            style={{ width: '300px' }}
-                            placeholder="Buscar"
-                            value={searchTerm}
-                            onChange={(e) => setSearchTerm(e.target.value)}
-                        />
-                        <Button
-                            label={loading ? 'Carregando...' : 'Consultar'}
-                            icon={loading ? 'pi pi-spin pi-spinner' : 'pi pi-search'}
-                            style={{ marginLeft: '10px', backgroundColor: '#0152a1', height: '34px', borderRadius: '10px' }}
-                            onClick={handleSearch}
-                            disabled={loading}
-                        />
-                        <Button
-                            label="Criar"
-                            icon="pi pi-plus"
-                            style={{ marginLeft: 'auto', backgroundColor: '#0152a1', borderRadius: '10px' }}
-                            onClick={handleCreateClick}
-                        />
-                    </div>
-                    <GenericTable 
-                        filteredItems={items} 
-                        emptyMessage="Nenhum Parceiro encontrado" 
-                        onCodeClick={handleCodeClick}
-                    />
-                </>
-            ) : view === 'create' ? (
-                <>
-                    <h1 style={{ color: '#0152a1' }}>{getTitle()}</h1>
-                    <TabView
-                            activeIndex={activeIndex}
-                            onTabChange={(e) => {
-                                // Verifica se o usuário está tentando acessar "Contatos" (índice 1)
-                                if (e.index === 1 && !codigo) {
-                                    toastError("Complete o cadastro em 'Dados Gerais' antes de acessar os contatos.");
-                                    return;
-                                }
-                                setActiveIndex(e.index); // Atualiza a aba ativa
-                            }}
-                        >
-                        <TabPanel header="Dados Gerais">
-                            <Fornecedores
-                                parceiroId={parceiroId} 
-                                onBackClick={handleBackClick} 
-                                onImageUploadClick={handleImageUploadClick}
-                                onCadastroConcluido={handleCadastroConcluido}
+        <>
+
+
+            <div style={{ display: 'flex', alignItems: 'center', marginBottom: '1.5rem', marginTop: '8px' }}>
+                <Button
+                    label="Criar"
+                    icon="pi pi-plus"
+                    style={{ marginLeft: 'auto', backgroundColor: '#0152a1' }}
+                    onClick={() => setModalVisible(true)}
+                />
+            </div>
+
+            <GenericTable 
+                emptyMessage="Nenhum Parceiro Contato encontrado"
+                filteredItems={filteredAgentes}
+                columns={agenteColumns}
+                onCodeClick={handleEditAgente}
+            />
+
+            <Dialog 
+                visible={modalVisible}
+                style={{ width: '60vw' }} 
+                onHide={() => {
+                    setModalVisible(false);
+                    setAgenteNome(''); // Limpa o nome ao fechar o modal, se necessário
+                }}
+            >   
+                <h1 style={{ color: '#0152a1' }}>
+                    {editing ? `Editar Fornecedor Tipo: ${agenteNome}` : 'Cadastro Fornecedor Tipo'}
+                </h1>
+                <form className="erp-form" onSubmit={handleSubmit}>
+                    {/* Primeira linha */}
+                    <div className="form-row">
+                        <div className="form-group">
+                            <label htmlFor="tpa_codigo">Código</label>
+                            <input
+                                style={{width:'200px',height:'37.6px'}}
+                                disabled
+                                type="text"
+                                id="tpa_codigo"
+                                name="tpa_codigo"
+                                value={request.tpa_codigo || ''}
+                                onChange={handleInputChange}
                             />
-                        </TabPanel>
-                        <TabPanel header="Contatos">
-                            {activeIndex === 1 && <FornecedoresContato parceiroId={parceiroId} />}
-                        </TabPanel>
-                    </TabView>
-                </>
-            ) : (
-                <ImageUpload agenciaId={parceiroId!} />
-            )}
-        </div>
+                        </div>
+                    </div>
+
+                    {/* Segunda linha */}
+                    <div className="form-row">
+                        <div className="form-group">
+                            <label htmlFor="tpa_descricao">Fornecedor Tipo</label>
+                            <input
+                                type="text"
+                                id="tpa_descricao"
+                                name="tpa_descricao"
+                                value={request.tpa_descricao || ''}
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="form-row">
+                            <Button
+                                label="Voltar"
+                                icon="pi pi-arrow-left"
+                                style={{backgroundColor: '#0152a1',width:'100px',height:'34px',marginLeft:'500px',borderRadius:'4px' }}
+                                onClick={closeDialog} // Chama a função passada como prop
+                                type="button"
+                            />
+                        {/* Condição para renderizar o botão de exclusão */}
+                        {request.tpa_codigo && (
+                        <button
+                            style={{marginLeft:'0px',color:'white',width:'100px'}}
+                            type="button"
+                            className="reset-btn"
+                            onClick={handleDeleteClick}
+                            disabled={loading}
+                        >
+                            <i className="fas fa-trash-alt"></i>{loading ? "Excluindo..." : "Excluir"}
+                        </button>
+                        )}
+                        
+                        <button
+                            style={{color:'white',backgroundColor:'#0152a1',marginLeft: request.tpa_codigo ? '14px' : '0px',display: request.tpa_codigo ? 'none' :''}}
+                            type="button"
+                            className="reset-btn"
+                            onClick={handleReset}
+                        >
+                            <i className="fas fa-trash-alt"></i> Limpar
+                        </button>
+                        <button style={{width:'100px',height:'34px',padding:'inherit'}} disabled={loading} type="submit" className="submit-btn"><i style={{marginRight:'10px'}}className="fas fa-save"></i>{loading ? 'Salvando...' : 'Salvar'}</button>
+                        <ConfirmDialog/>
+                    </div>
+                </form>
+            </Dialog>
+        </>
     );
 };
 
-export default FornecedoresTipo;
+export default ForncedoresTipo;
