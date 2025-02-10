6200df936fca15709b7169be860b4188d3c9c35c
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-10 17:09:15 -0300

    Update SubGrupo.tsx

--- src/components/specific/SubGrupo.tsx
+++ src/components/specific/SubGrupo.tsx
@@ -231,7 +231,7 @@ const SubGrupo: React.FC = ({ onBackClick }) => {
                     <Button
                         label="Voltar"
                         icon="pi pi-arrow-left"
-                        style={{backgroundColor: '#0152a1',width:'100px',height:'34px',marginLeft:'580px',borderRadius:'4px' }}
+                        style={{backgroundColor: '#0152a1',width:'100px',height:'34px',marginLeft:'680px',borderRadius:'4px' }}
                         onClick={onBackClick} // Chama a função passada como prop
                     />
                 {/* Condição para renderizar o botão de exclusão */}
