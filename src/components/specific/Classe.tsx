97456fcbc27c7ba6fec8cb9254a37b4cb169a5eb
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-11 15:53:17 -0300

    Update Classe.tsx

--- src/components/specific/Classe.tsx
+++ src/components/specific/Classe.tsx
@@ -249,7 +249,7 @@ const Classe: React.FC = ({ onBackClick }) => {
                     onClick={handleDeleteClick}
                     disabled={loading}
                 >
-                    <i className="fas fa-trash-alt"></i>{loading ? "Excluindo..." : "Excluir"}
+                    <i className="fas fa-trash-alt"></i>Excluir
                 </button>
                 )}
                 
