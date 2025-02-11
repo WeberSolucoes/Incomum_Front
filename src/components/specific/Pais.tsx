c5545aa3a4dc8a3fb0360ab142334ce276069e7f
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-11 15:59:31 -0300

    Update Pais.tsx

--- src/components/specific/Pais.tsx
+++ src/components/specific/Pais.tsx
@@ -223,7 +223,7 @@ const Pais: React.FC = ({onBackClick}) => {
                     onClick={handleDeleteClick}
                     disabled={loading}
                 >
-                    <i className="fas fa-trash-alt"></i>{loading ? "Excluindo..." : "Excluir"}
+                    <i className="fas fa-trash-alt"></i>Excluir
                 </button>
                 )}
                 
