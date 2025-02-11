600aa46fa39eb2e610f92d839ff7518934d07a78
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-11 16:00:41 -0300

    Update SituacaoTuristico.tsx

--- src/components/specific/SituacaoTuristico.tsx
+++ src/components/specific/SituacaoTuristico.tsx
@@ -224,7 +224,7 @@ const SituacaoTuristico: React.FC = ({ onBackClick }) => {
                     onClick={handleDeleteClick}
                     disabled={loading}
                 >
-                    <i className="fas fa-trash-alt"></i>{loading ? "Excluindo..." : "Excluir"}
+                    <i className="fas fa-trash-alt"></i>Excluir
                 </button>
                 )}
                 
