7d38e3134473b0c71c024120cddfa3529b9655d3
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-11 15:52:44 -0300

    Update Cidade.tsx

--- src/components/specific/Cidade.tsx
+++ src/components/specific/Cidade.tsx
@@ -263,7 +263,7 @@ const Cidade: React.FC = ({ onBackClick }) => {
                     onClick={handleDeleteClick}
                     disabled={loading}
                 >
-                    <i className="fas fa-trash-alt"></i>{loading ? "Excluindo..." : "Excluir"}
+                    <i className="fas fa-trash-alt"></i>Excluir
                 </button>
                 )}
                 
