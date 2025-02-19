e04f58c627096cf2134940cb1dccb59ea5d74c0a
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-19 10:01:28 -0300

    Update TipoRegime.tsx

--- src/components/specific/TipoRegime.tsx
+++ src/components/specific/TipoRegime.tsx
@@ -14,7 +14,7 @@ import { RootState } from "../../hooks/store";
 
 
 const TipoRegime: React.FC = ({ onBackClick }) => {
-    const { codigo } = useCodigo(); // Assumindo que useCodigo fornece o código da unidade
+    const { setCodigo,codigo } = useCodigo(); // Assumindo que useCodigo fornece o código da unidade
     const [request, setRequest] = useState<RegimeCreateRequest>({} as RegimeCreateRequest);
     const [rua, setRua] = useState('');
     const [numero, setNumero] = useState('');
@@ -31,8 +31,12 @@ const TipoRegime: React.FC = ({ onBackClick }) => {
     const activeTab = useSelector((state: RootState) => state.tabs.activeTab);
 
     useEffect(() => {
+        if (!activeTab || activeTab !== 'Regime') {
+            // Reseta o código se a aba não for "Agência"
+            setCodigo(null);
+            return; // Não executa a consulta
+        }
         if (!codigo) return; // 🔍 Evita rodar com código inválido
-        if (!activeTab) return; // 🔍 Espera até `activeTab` estar definido
         if (activeTab !== 'Regime') return; // 🔍 Só roda na aba certa
 
         console.log("✅ Buscando dados para código:", codigo);
