42eaa5e65ea8f6141342989f402f9dd3dcc92f13
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-19 11:16:23 -0300

    Update MoedaList.tsx

--- src/components/specific/MoedaList.tsx
+++ src/components/specific/MoedaList.tsx
@@ -16,9 +16,25 @@ const MoedaList: React.FC = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [view, setView] = useState<'list' | 'create'>('list'); // Estado para controlar a visualização atual
     const [loading, setLoading] = useState(false); // Estado de carregamento
+    const [descricaoSelecionada, setDescricaoSelecionada] = useState<string | null>(null); // Estado para a descrição
 
     const { codigo,setCodigo } = useCodigo(); // Acesso ao contexto
 
+ 
+    const getTitle = () => {
+        const maxLength = 27;
+        const truncatedDescricao = descricaoSelecionada 
+            ? descricaoSelecionada.length > maxLength 
+                ? descricaoSelecionada.slice(0, maxLength) + '...' 
+                : descricaoSelecionada 
+            : '';
+        
+        return truncatedDescricao 
+            ? `Cadastro Moeda - ${truncatedDescricao}` 
+            : 'Cadastro Moeda'; // Título padrão se não houver descrição
+    };   
+
+    
     const handleSearch = async () => {
         if (searchTerm.length < 3) {
             toastError('Por favor, insira pelo menos 3 caracteres para realizar a pesquisa.');
@@ -53,16 +69,22 @@ const MoedaList: React.FC = () => {
     useEnterKey(handleSearch);
 
     const handleCodeClick = (codigo: number) => {
+        const agencia = items.find(item => item.codigo === codigo); // Encontre a agência selecionada
+        if (agencia) {
+            setDescricaoSelecionada(agencia.descricao); // Atualiza a descrição selecionada
+        }
         setCodigo(codigo);
-        setView('create'); // Muda para a visualização de edição
+        setView('create'); // Abre a view de cadastro ao selecionar
     };
 
     const handleCreateClick = () => {
         setCodigo(null); // Resetando o código para criar uma nova unidade
         setView('create'); // Muda para a visualização de criação
+        setDescricaoSelecionada(null); 
     };
 
     const handleBackClick = () => {
+        setDescricaoSelecionada(null); 
         setView('list'); // Volta para a visualização da lista
         window.scrollTo({
             top: 0,  // Define a posição do topo da página
@@ -112,7 +134,7 @@ const MoedaList: React.FC = () => {
                 </>
             ) : (
                 <>
-                    <h1 style={{color:'#0152a1'}}>{codigo === null ? 'Cadastro Moeda' : `Cadastro Moeda - ${paisDescricao}`}</h1>
+                    <h1 style={{color:'#0152a1'}}>{getTitle()}</h1>
                     <Moeda onBackClick={handleBackClick} /> {/* Renderiza o componente de cadastro/edição */}
                 </>
             )}
