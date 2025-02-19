b0d8ce8f06a2103973c30fbe698936715d02bdf7
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-19 09:51:00 -0300

    Update Pais.tsx

--- src/components/specific/Pais.tsx
+++ src/components/specific/Pais.tsx
@@ -12,7 +12,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../hooks/store";
 
 const Pais: React.FC = ({onBackClick}) => {
-    const { codigo } = useCodigo(); // Assumindo que useCodigo fornece o código da unidade
+    const { setCodigo,codigo } = useCodigo(); // Assumindo que useCodigo fornece o código da unidade
     const [request, setRequest] = useState<PaisCreateRequest>({} as PaisCreateRequest);
     const [rua, setRua] = useState(''); 
     const [numero, setNumero] = useState('');
@@ -28,8 +28,12 @@ const Pais: React.FC = ({onBackClick}) => {
     const activeTab = useSelector((state: RootState) => state.tabs.activeTab);
 
     useEffect(() => {
+        if (!activeTab || activeTab !== 'Pais') {
+            // Reseta o código se a aba não for "Agência"
+            setCodigo(null);
+            return; // Não executa a consulta
+        }
         if (!codigo) return; // 🔍 Evita rodar com código inválido
-        if (!activeTab) return; // 🔍 Espera até `activeTab` estar definido
         if (activeTab !== 'Pais') return; // 🔍 Só roda na aba certa
 
         console.log("✅ Buscando dados para código:", codigo);
