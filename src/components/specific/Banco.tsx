33f67669a0b32168c81a94999c63c1bf3289a83e
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-02-21 16:16:44 -0300

    Update Banco.tsx

--- src/components/specific/Banco.tsx
+++ src/components/specific/Banco.tsx
@@ -201,7 +201,7 @@ const Banco: React.FC = ({ onBackClick }) => {
                         disabled
                     />
                 </div>
-                <div className="form-group">
+                <div className="form-group" style={{marginLeft:'-600px'}}>
                     <label htmlFor="dep_codigo">Codigo Bancario</label>
                     <input
                         type="text"
