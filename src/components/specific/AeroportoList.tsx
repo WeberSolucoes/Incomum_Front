e5f84947202f07d4f88f7630648353d78a443ee4
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-03-12 14:05:54 -0300

    Update AeroportoList.tsx

--- src/components/specific/AeroportoList.tsx
+++ src/components/specific/AeroportoList.tsx
@@ -65,11 +65,11 @@ const AeroportoList: React.FC = ({ isActive, state }) => {
         }
     };
 
-    const handleEnterPress = () => {
-        console.log('Enter pressionado na aba ativa!');
-    };
-
-    useEnterKey(handleEnterPress, isActive, buttonRef);
+    useEnterKey(() => {
+        if (isActive && !loading) {
+            handleSearch();
+        }
+    }, isActive, buttonRef);
 
     const handleCodeClick = (codigo: number) => {
         const agencia = items.find(item => item.codigo === codigo); // Encontre a agência selecionada
