ece97161edb89a0accda86937356d837d9aa7492
kauantg0304 <105811366+kauantg0304@users.noreply.github.com>
2025-03-27 15:55:50 -0300

    Update store.tsx

--- src/hooks/store.tsx
+++ src/hooks/store.tsx
@@ -1,11 +1,18 @@
+// store.ts
 import { configureStore } from '@reduxjs/toolkit';
 import tabsReducer from './tabSlice';
+import agenciaReducer from './agenciaSlice';
+import genericListReducer from './genericListSlice';
 
 export const store = configureStore({
   reducer: {
     tabs: tabsReducer,
+    agencia: agenciaReducer,
+    genericList: genericListReducer, // Adicione o slice genérico ao store
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export default store;
